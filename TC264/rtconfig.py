4a0c2003c772498a61e47e8b70f8baf69f540066
liukang <liukang@rt-thread.com>
2021-03-18 14:51:38 +0800

    【提交】 scons project.

---
+++ TC264/rtconfig.py
@@ -0,0 +1,37 @@
+import os
+
+# toolchains options
+ARCH='tricore'
+CPU='tc26xb'
+CROSS_TOOL='gcc'
+
+if os.getenv('RTT_CC'):
+	CROSS_TOOL = os.getenv('RTT_CC')
+
+if  CROSS_TOOL == 'gcc':
+	PLATFORM 	= 'gcc'
+	EXEC_PATH 	= 'C:/Program Files (x86)/CodeSourcery/Sourcery G++ Lite/bin'
+
+if os.getenv('RTT_EXEC_PATH'):
+	EXEC_PATH = os.getenv('RTT_EXEC_PATH')
+
+BUILD = 'debug'
+EXEC_PATH = r"D:/software/AURIX-Studio-1.2.2/plugins/com.infineon.aurix.tools_1.2.2/build_system/tools/Compilers/Tasking_1.1r4/ctc/bin"
+
+if PLATFORM == 'gcc':
+    # toolchains
+    PREFIX = 'cctc'
+    CC = PREFIX + ''
+    AS = PREFIX + ''
+    AR = PREFIX + ''
+    LINK = PREFIX + ''
+    TARGET_EXT = 'elf'
+    OBJDUMP = PREFIX + ''
+    OBJCPY = EXEC_PATH + 'elfsize'
+
+    DEVICE = ''
+    CFLAGS = DEVICE
+    AFLAGS =  DEVICE
+    LFLAGS = DEVICE + ' - lrt - lfp_fpu - lcs_fpu - Wl - Oc - Wl - OL - Wl - Ot - Wl - Ox - Wl - Oy - Wl - -map - file = "TC364.map" - Wl - mc - Wl - mf - Wl - mi - Wl - mk - Wl - ml - Wl - mm - Wl - md - Wl - mr - Wl - mu - -no - warnings = -Wl - -error - limit = 42 - -strict - -nachronisms - -force - c + + -Ctc36x - o rtthread.elf - Wl - o rtthread.hex:IHEX - -lsl - core = vtc - -lsl - file =../ Lcf_Tasking_Tricore_Tc.lsl $(OBJS)'
+
+    POST_ACTION = OBJCPY + 'rtthread.elf\n'
---
+++ TC377/USER/Cpu0_Main.c
@@ -0,0 +1,72 @@
+#include "Cpu0_Main.h"
+#include "headfile.h"
+#include "rtthread.h"
+
+#pragma section all "cpu0_dsram"
+
+uint16 num;
+
+int core0_main(void)
+{
+extern int rtthread_startup(void);
+
+    disableInterrupts();
+    get_clk();
+
+    rtthread_startup();
+
+    rt_kprintf("Failed to start rt-thread.\n");
+
+    while(1);
+
+}
+
+void GPIO_Demo_init(void)
+{
+    /* configure P20.8 as general output */
+    IfxPort_setPinMode(&MODULE_P20, 8,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P20.9 as general output */
+    IfxPort_setPinMode(&MODULE_P20, 9,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P21.4 as general output */
+    IfxPort_setPinMode(&MODULE_P21, 4,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P21.5 as general output */
+    IfxPort_setPinMode(&MODULE_P21, 5,  IfxPort_Mode_outputPushPullGeneral);
+}
+
+static rt_uint8_t led_thread_stack[2048];
+struct rt_thread led_thread_thread;
+void led_thread_entry(void *parameter)
+{
+    GPIO_Demo_init();
+    while(1)
+    {
+        IfxPort_togglePin(&MODULE_P20, 8);
+        IfxPort_togglePin(&MODULE_P20, 9);
+        rt_thread_mdelay(1000);
+        IfxPort_togglePin(&MODULE_P21, 4);
+        IfxPort_togglePin(&MODULE_P21, 5);
+    }
+}
+
+
+int main(void)
+{
+    rt_thread_t tid;
+    rt_err_t result;
+
+    rt_uint32_t count;
+
+    tid = &led_thread_thread;
+    result = rt_thread_init(tid, "led", led_thread_entry, RT_NULL,
+                led_thread_stack, sizeof(led_thread_stack), 10, 20);
+    RT_ASSERT(result == RT_EOK);
+    rt_thread_startup(tid);
+    while(1)
+    {
+        rt_thread_mdelay(1000);
+        rt_kprintf("hello rt-thread! %d\n",count++);
+    }
+}
+
+
+#pragma section all restore
